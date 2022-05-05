32b84714a4e9f8d9b980fdcb0abaca1bc616ce84
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-05 18:25:28 +0600

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Yuri Gerasimchuk on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
