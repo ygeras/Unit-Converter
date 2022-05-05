2dab1eeac8b40d1cc80a9925a874c2879f19f934
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-05 19:08:28 +0600

    Unit Converter: Basic functionality for distance

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,14 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State private var input = 0.0
+    @State private var inputUnit = "Km"
+    @State private var outputUnit = "Km"
+    
+    let units = ["Km", "Ft", "Yd", "Ml"]
+    
+    var result: String {
+        let inputToMetersMultiplier: Double
+        let metersToOutputMultiplier: Double
+        
+        switch inputUnit {
+        case "Km":
+            inputToMetersMultiplier = 1000
+        case "Ft":
+            inputToMetersMultiplier = 0.3048
+        case "Yd":
+            inputToMetersMultiplier = 0.9144
+        case "Ml":
+            inputToMetersMultiplier = 1609.34
+        default:
+            inputToMetersMultiplier = 1
+        }
+        
+        switch outputUnit {
+        case "Km":
+            metersToOutputMultiplier = 0.001
+        case "Ft":
+            metersToOutputMultiplier = 3.28084
+        case "Yd":
+            metersToOutputMultiplier = 1.09361
+        case "Ml":
+            metersToOutputMultiplier = 0.000621371
+        default:
+            metersToOutputMultiplier = 1.0
+        }
+        
+        let inputMeters = input * inputToMetersMultiplier
+        let output = inputMeters * metersToOutputMultiplier
+        
+        let outputString = output.formatted()
+        return "\(outputString) \(outputUnit.lowercased())"
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $input, format: .number)
+                } header: {
+                    Text("Enter amount here")
+                }
+                
+                Section {
+                    Picker("Unit from", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("From unit")
+                }
+                
+                Section {
+                    Picker("Unit from", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To unit")
+                }
+                
+                Section {
+                    Text(result)
+                } header: {
+                    Text("Result")
+                }
+                
+            }
+            .navigationTitle("Unit Converter")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
